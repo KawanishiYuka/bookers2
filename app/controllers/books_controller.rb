16db934d2a64e122237f9783e7a08a33af07247b
EC2 Default User <ec2-user@ip-172-31-54-223.ec2.internal>
2021-11-23 14:38:24 +0000

    [Update]indexページの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,16 @@
 class BooksController < ApplicationController
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def index
-    @book = Book.new
     @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -19,5 +24,12 @@ class BooksController < ApplicationController
 
   def destroy
   end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
   
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1a009d9ce105a3d4134ec25ff337cb3d458488d683e05e9eb89fe32a7b5bd03cd4976bff850a7373184a01fa7da3275384857d20ee6c81cf9c3605e700228da6'
