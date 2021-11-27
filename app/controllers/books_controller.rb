c41ba9ba3857a62de900bdb79c6c92fe83a1fb84
EC2 Default User <ec2-user@ip-172-31-54-223.ec2.internal>
2021-11-27 09:10:48 +0000

    [Add]フラッシュメッセージ機能・アクセス制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,14 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+     flash[:notice] = "You have created book successfully."
+     redirect_to book_path(@book.id)
+    else
+     @user = current_user
+     @books = Book.page(params[:page]).reverse_order
+     render :index
+    end  
   end
 
   def index
@@ -20,12 +26,28 @@ class BooksController < ApplicationController
   end
   
   def edit
+    @book_detail = Book.find(params[:id])
+    if @book_detail == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
   
   def update
+    @book_detail = Book.find(params[:id])
+    if @book_detail.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book_detail.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book_detail = Book.find(params[:id])
+    @book_detail.destroy
+    redirect_to books_path
   end
   
   private
